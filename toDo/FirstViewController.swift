580f0e6c36fc0c43c789a3b6e21f836e9ff8bd95
hugo <hugomartinezf@gmail.com>
2018-05-11 10:43:54 -0500

    Initial Commit

---
+++ toDo/FirstViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FirstViewController.swift
+//  toDo
+//
+//  Created by Hugo on 3/14/18.
+//  Copyright © 2018 ocelot. All rights reserved.
+//
+
+import UIKit
+
+var list = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"]
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        let cell =  UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return (cell)
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
